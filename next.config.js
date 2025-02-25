e38acb53c830babe1c47cf1e6e2ae49de260f2f0
Muynuddin R <muynuddinr@gmail.com>
2025-02-25 11:15:36 +0530

    blog done both frontend and backend

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: [
+      'localhost',
+      'images.unsplash.com',
+      'unsplash.com',
+      'plus.unsplash.com'
+    ],
+  },
   eslint: {
     ignoreDuringBuilds: false,
     dirs: ['src']
